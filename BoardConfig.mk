7978df5dab7340d474bc39a69d35e220b063dceb
Hugo Snake <snakehugo@gmx.de>
2022-04-23 17:17:02 +0200

    Include device/qcom/sepolicy-legacy/sepolicy.mk
    Change-Id: If6492d387d163c7461244243608b4d15a90db2b2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -251,6 +251,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/legacy
 
